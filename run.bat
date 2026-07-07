1f27bf5be6306fb8acf8286d4c02cff4b0127389
diept31 <t.diepenmaat@hhsk.nl>
2026-07-07 08:26:49 +0200

    Update run.bat

--- run.bat
+++ run.bat
@@ -1,10 +1,3 @@
 
-@echo off
-
-start "" /B "C:\Users\Diepenmaat\AppData\Local\miniconda3\envs\hoogtepuzzel\python.exe" -m streamlit run app.py
-
-#timeout /t 15 >nul
-
-#start http://localhost:8501
 
 
